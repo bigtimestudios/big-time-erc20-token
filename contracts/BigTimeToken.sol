5a34a06ba8ebdca6abacb86560fb76a40e2b9907
Kevin Lin <kevinlin@openloot.com>
2023-07-25 13:03:33 -0700

    put back owner for initial mint

--- contracts/BigTimeToken.sol
+++ contracts/BigTimeToken.sol
@@ -7,8 +7,9 @@ contract BigTimeToken is ERC20 {
     constructor(
         string memory _name,
         string memory _symbol,
-        uint _totalSupply
+        uint _totalSupply,
+        address _owner
     ) ERC20(_name, _symbol) {
-        _mint(msg.sender, _totalSupply * 10 ** decimals());
+        _mint(_owner, _totalSupply * 10 ** decimals());
     }
 }
